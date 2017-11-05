191ed5fd5988d01a4cfe963a68ca99c914b27289
andrevar <32806468+andrevar@users.noreply.github.com>
2017-11-06 00:56:46 +0100

    Create Memoized_Fibonacci.js

---
+++ 5 kyu/Memoized_Fibonacci.js
@@ -0,0 +1,17 @@
+var fibonacci = function(m) {
+    var cache = {};
+function fibo (n){
+    var fib;
+    if(n==0 || n == 1) {
+        fib = n;
+    } else if (n in cache) {
+        fib = cache[n];  
+    } else {
+        fib = fibo(n-1) + fibo(n-2);
+        cache[n] = fib;
+        
+    }
+    return fib;
+}
+   return fibo(m); 
+}
