35135901b8dbb75702ab4d7b4a3a31d462d48f7b
andrevar <32806468+andrevar@users.noreply.github.com>
2017-11-06 00:39:00 +0100

    Create Multiplying_numbers_as_strings.js

---
+++ 3 kyu/Multiplying_numbers_as_strings.js
@@ -0,0 +1,36 @@
+function multiply(a, b) {
+    if(a == '0' || b == '0') {
+        return '0';
+    }
+    if (a.length > b.length) {
+        for (var x = 0; x < a.length - b.length; x++) {
+            b = '0' + b;
+        }
+    } else if (a.length < b.length) {
+        for (var y = 0; y < b.length - a.length; y++) {
+            a = '0' + a;
+        }
+    }
+    a = a.split('').reverse();
+    b = b.split('').reverse();
+    var arr = [];
+    for (var i = 0; i < a.length; i++) {
+        for (var z = 0; z < b.length; z++) {
+            if(!arr[i + z]) {
+                arr[i + z] = 0
+            }
+            arr[i + z] += parseInt(a[i]) * parseInt(b[z]);
+        }
+    }
+    arr.push(0);
+    arr = arr.map(function(a , index) {
+        arr[index + 1] += parseInt(a / 10);
+        a = a % 10;
+        return a;
+    })
+    arr = arr.reverse();
+    while(arr[0] == 0) {
+        arr.shift();
+    }
+return arr.join('');
+}
