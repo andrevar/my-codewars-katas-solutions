9b80bf848620ecc49192e593ca988bda1a9ede1f
andrevar <32806468+andrevar@users.noreply.github.com>
2017-11-17 20:39:43 +0100

    Create SpeedCode_#3_×_Fun_with_ES6_Classes_#5-Dogs_and_Classes.js

---
+++ 8 kyu/SpeedCode_#3_×_Fun_with_ES6_Classes_#5-Dogs_and_Classes.js
@@ -0,0 +1,10 @@
+function Labrador(name,age,gender,master){
+ this.name=name;
+ this.age=age;
+ this.gender=gender;
+ this.species="Labrador";
+ this.legs=4;
+ this.size="Large";
+ this.master=master;
+ this.loyal=true;
+}
