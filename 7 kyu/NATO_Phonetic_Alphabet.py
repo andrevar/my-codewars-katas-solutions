96507adf5fd84fca71cbdec74fd252fb1fc08860
andrevar <32806468+andrevar@users.noreply.github.com>
2017-11-25 15:20:29 +0100

    Create NATO_Phonetic_Alphabet.py

---
+++ 7 kyu/NATO_Phonetic_Alphabet.py
@@ -0,0 +1,14 @@
+letters =  {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  }
+  
+def nato(word):
+    return ' '.join(list(map(lambda i: letters[i], [j for j in word.upper()])))
